d053edf90da44178a09be6f2e9856e0ae171215b
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-03-30 17:20:17 +0100

    added the subnets local block to loop through it and grab the private subnet ids

--- Terraform/modules/ecs/locals.tf
+++ Terraform/modules/ecs/locals.tf
@@ -1,3 +1,24 @@
 locals {
     name = "api-gateway"
 }
+
+locals {
+  subnets = {
+    "public-1"  = { cidr = "10.0.1.0/24", az = "eu-north-1a", public = true  }
+    "public-2"  = { cidr = "10.0.2.0/24", az = "eu-north-1b", public = true  }
+    "private-1" = { cidr = "10.0.3.0/24", az = "eu-north-1a", public = false }
+    "private-2" = { cidr = "10.0.4.0/24", az = "eu-north-1b", public = false }
+    "private-3" = { cidr = "10.0.5.0/24", az = "eu-north-1c", public = false }
+    "public-3" = { cidr = "10.0.6.0/24", az = "eu-north-1c", public = true }
+  }
+}
+
+# loops through the locals block above and determines wether it is a private subnet, 
+# i can then reference the private subnets like "locals.private_subnet_ids" for ecs service 
+locals {
+  private_subnet_ids = [
+    for k, subnet in aws_subnet.subnets :
+    subnet.id
+    if local.subnets[k].public == false
+  ]
+}
