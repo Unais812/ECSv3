600066a2a3962ac0bb44b0ab79b032545c8b592e
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-03-30 16:48:57 +0100

    added modules

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,3 +7,14 @@ module "secret" {
   db_password = var.db_password
   jwt_secret  = var.jwt_secret
 }
+
+module "ecs" {
+  source = "./modules/ecs"
+  execution_role_arn = module.iam.execution_role_arn
+  api_gateway_target_group = module.alb.api_gateway_target_group
+}
+
+module "alb" {
+  source = "./modules/alb"
+  vpc_id = module.vpc.vpc_id
+}
