3f6ee8bb0c5672ffd72cbd2dd84e8de8281bd2b6
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-05-12 22:52:39 +0100

    changed the instance size to t3.small, other one wasnt supported in my region

--- Terraform/modules/observability/variables.tf
+++ Terraform/modules/observability/variables.tf
@@ -1,11 +1,7 @@
 variable "instance_type" {
   description = "instance type for the ec2 instance"
   type = string
-  default = "t2.micro"
-}
-
-variable "private_subnet_ids" {
-  description = "ids of the private subnets"
+  default = "t3.small"
 }
 
 variable "vpc_cidr" {
@@ -16,5 +12,26 @@ variable "vpc_cidr" {
 
 variable "vpc_id" {
   description = "id of vpc"
+  type = string
+}
+
+variable "public_subnet_id" {
+  description = "public subnet ids for nat"
+  type = string
+}
+
+variable "igw_id" {
+  description = "id of igw"
+  type = string
+}
+
+variable "public_cidr" {
+  description = "cidr for public access"
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "ecs_sg" {
+  description = "id of the ecs sg allowing ecs tasks to accept traffic from instance"
   type = string
 }
