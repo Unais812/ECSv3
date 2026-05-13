d3b602aca79b0d3d13a0c93b59a608a9ee94e06b
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-05-13 17:59:40 +0100

    added alb dns name zone id for route53 record creation

--- Terraform/modules/alb/outputs.tf
+++ Terraform/modules/alb/outputs.tf
@@ -5,3 +5,11 @@ output "api_gateway_target_group" {
 output "dashboard_api_target_group" {
   value = aws_lb_target_group.dashboard-api.arn
 }
+
+output "alb_dns" {
+  value = aws_lb.ecs-v3-alb.dns_name
+}
+
+output "alb_zone_id" {
+  value = aws_lb.ecs-v3-alb.zone_id
+}
